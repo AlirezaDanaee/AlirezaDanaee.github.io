89f7a4de18cdf441b213fe0089840d9ebbb47dcf
AlirezaDanaee <93105102+AlirezaDanaee@users.noreply.github.com>
2022-05-08 22:46:09 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ let particleCount;
 if (innerWidth >= innerHeight) {
 	particleCount = parseInt(innerWidth / 25);
 } else {
-	particleCount = parseInt(innerHeight / 30);
+	particleCount = parseInt(innerHeight / 15);
 }
 let particleObjects = [];
 let connectorObjects = [];
